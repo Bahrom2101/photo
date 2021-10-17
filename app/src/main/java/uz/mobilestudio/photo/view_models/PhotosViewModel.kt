d8a83083c90d5914c5e9ef77bd80b649004e02cf
bahrom <jabborov0121@gmail.com>
2021-10-17 21:49:50 +0500

    editing photo added

--- app/src/main/java/uz/mobilestudio/photo/view_models/PhotosViewModel.kt
+++ app/src/main/java/uz/mobilestudio/photo/view_models/PhotosViewModel.kt
@@ -5,9 +5,7 @@ import androidx.lifecycle.ViewModel
 import uz.mobilestudio.photo.models.api.all_photos.Photo
 import uz.mobilestudio.photo.models.api.search.SearchResponse
 import uz.mobilestudio.photo.repositories.PhotosRepository
-import uz.mobilestudio.photo.retrofit.Common.ACCESS_KEY1
-import uz.mobilestudio.photo.retrofit.Common.ACCESS_KEY2
-import uz.mobilestudio.photo.retrofit.Common.ACCESS_KEY3
+import uz.mobilestudio.photo.retrofit.Common.ACCESS_KEYS
 import uz.mobilestudio.photo.retrofit.Common.PER_PAGE
 import kotlin.random.Random
 
@@ -16,72 +14,25 @@ class PhotosViewModel : ViewModel() {
     private val photosRepository = PhotosRepository()
 
     fun getPhotos(page: Int): LiveData<List<Photo>> {
-        return when (Random.nextInt(1, 4)) {
-            1 -> {
-                photosRepository.getPhotos(ACCESS_KEY1, page, PER_PAGE)
-            }
-            2 -> {
-                photosRepository.getPhotos(ACCESS_KEY2, page, PER_PAGE)
-            }
-            3 -> {
-                photosRepository.getPhotos(ACCESS_KEY3, page, PER_PAGE)
-            }
-            else -> {
-                photosRepository.getPhotos(ACCESS_KEY3, page, PER_PAGE)
-            }
-        }
+        return photosRepository.getPhotos(ACCESS_KEYS[Random.nextInt(0, 8)], page, PER_PAGE)
     }
 
     fun getPopularPhotos(page: Int): LiveData<List<Photo>> {
-        return when (Random.nextInt(1, 4)) {
-            1 -> {
-                photosRepository.getPhotos(ACCESS_KEY1, page, PER_PAGE, "popular")
-            }
-            2 -> {
-                photosRepository.getPhotos(ACCESS_KEY2, page, PER_PAGE, "popular")
-            }
-            3 -> {
-                photosRepository.getPhotos(ACCESS_KEY3, page, PER_PAGE, "popular")
-            }
-            else -> {
-                photosRepository.getPhotos(ACCESS_KEY3, page, PER_PAGE, "popular")
-            }
-        }
+        return photosRepository.getPhotos(
+            ACCESS_KEYS[Random.nextInt(0, 8)],
+            page,
+            PER_PAGE,
+            "popular"
+        )
     }
 
     fun getRandomPhoto(): LiveData<Photo> {
-        return when (Random.nextInt(1, 4)) {
-            1 -> {
-                photosRepository.getRandomPhoto(ACCESS_KEY1)
-            }
-            2 -> {
-                photosRepository.getRandomPhoto(ACCESS_KEY2)
-            }
-            3 -> {
-                photosRepository.getRandomPhoto(ACCESS_KEY3)
-            }
-            else -> {
-                photosRepository.getRandomPhoto(ACCESS_KEY3)
-            }
-        }
+        return photosRepository.getRandomPhoto(ACCESS_KEYS[Random.nextInt(0, 8)])
     }
 
 
-    fun getSearchedPhotos(page: Int,query:String): LiveData<SearchResponse> {
-        return when (Random.nextInt(1, 4)) {
-            1 -> {
-                photosRepository.getSearchedPhotos(ACCESS_KEY1, page, PER_PAGE, query)
-            }
-            2 -> {
-                photosRepository.getSearchedPhotos(ACCESS_KEY2, page, PER_PAGE, query)
-            }
-            3 -> {
-                photosRepository.getSearchedPhotos(ACCESS_KEY3, page, PER_PAGE, query)
-            }
-            else -> {
-                photosRepository.getSearchedPhotos(ACCESS_KEY3, page, PER_PAGE, query)
-            }
-        }
+    fun getSearchedPhotos(page: Int, query: String): LiveData<SearchResponse> {
+        return photosRepository.getSearchedPhotos(ACCESS_KEYS[Random.nextInt(0, 8)], page, PER_PAGE, query)
     }
 
 }
